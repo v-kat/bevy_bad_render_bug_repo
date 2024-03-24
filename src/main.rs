183e1dcfa822e91bbba538a7ec3e0cd26deeb56c
Ivy <fake@email.com>
2024-03-24 10:19:57 -0700

    pure bevy with bevy_sprite same issue

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-use bevy::prelude::*;
-
 use bevy::asset::AssetMetaCheck;
-use bevy_egui::{egui, EguiContexts, EguiPlugin};
-use egui::{CentralPanel, Color32};
-
-pub const UV_RECT: egui::Rect =
-    egui::Rect::from_min_max(egui::pos2(0.0, 0.0), egui::pos2(1.0, 1.0));
+use bevy::prelude::*;
 
 fn main() {
     App::new()
@@ -18,31 +12,22 @@ fn main() {
                 }),
                 ..default()
             }),
-            EguiPlugin,
         ))
-        .add_systems(Update, home_menu_system)
+        .add_systems(Startup, setup)
+        .add_systems(Update, test_system)
         .run();
 
 }
 
-fn home_menu_system(
-    mut contexts: EguiContexts,
-    asset_server: Res<AssetServer>,
-) {
-    let img_handle = asset_server.load("tester.webp");
-    let background_texture_id = contexts.add_image(img_handle);
-    let screen_rect = contexts.ctx_mut().screen_rect();
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
 
-    CentralPanel::default().show(contexts.ctx_mut(), |ui| {
-        ui.vertical_centered(|ui| {
-            ui.painter()
-                .image(background_texture_id, screen_rect, UV_RECT, Color32::WHITE);
-            ui.add_space(screen_rect.max.y * 0.5);
-            if ui.button("Test").clicked() {
-            }
-            ui.add_space(screen_rect.max.y * 0.025);
-            if ui.button("Screen").clicked() {
-            };
-        });
+fn test_system(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let img_handle = asset_server.load("tester.webp");
+
+    commands.spawn(SpriteBundle {
+        texture: img_handle,
+        ..default()
     });
 }
